35e1e8a376667993a0a651ff3bd25a110a42c4f8
KristinLind <kristin.lind@comcast.net>
2026-01-21 14:35:21 -0700

    updates

--- controllers/invController.js
+++ controllers/invController.js
@@ -7,33 +7,34 @@ const invController = {}
 invController.buildByClassificationId = async function (req, res, next) {
   const classificationId = req.params.classificationId
 
-  // get inventory rows for that classification
+  const nav = await utilities.getNav()
   const data = await invModel.getInventoryByClassificationId(classificationId)
-
-  // build the HTML grid
   const grid = await utilities.buildClassificationGrid(data.rows)
-
-  // classification name for title
   const className = data.rows[0]?.classification_name || "Vehicles"
 
   res.render("inventory/classification", {
     title: className,
+    nav,
     grid,
   })
 }
 
 invController.buildByInventoryId = async function (req, res, next) {
   const invId = req.params.invId
+
+  const nav = await utilities.getNav()
   const vehicle = await invModel.getInventoryById(invId)
 
-  if (!vehicle) {
-    return next({ status: 404, message: "Vehicle not found." })
-  }
+  if (!vehicle) return next({ status: 404, message: "Vehicle not found." })
 
   const title = `${vehicle.inv_make} ${vehicle.inv_model}`
   const detailHtml = utilities.buildVehicleDetail(vehicle)
 
-  res.render("inventory/detail", { title, detailHtml })
+  res.render("inventory/detail", {
+    title,
+    nav,
+    detailHtml,
+  })
 }
 
 module.exports = invController
